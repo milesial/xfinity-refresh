0d6caceb0eb4fb1d8838d54fb5e850bebee4ffad
milesial <milesial@users.noreply.github.com>
2020-03-13 14:26:26 -0700

    Fix interface as argument

--- xfinity_refresh/__main__.py
+++ xfinity_refresh/__main__.py
@@ -1,11 +1,16 @@
 from multiprocessing import Process
 from elevate import elevate
+from argparse import ArgumentParser
 from xfinity_refresh.activate_pass import activate_pass
 from xfinity_refresh.change_mac import change_mac
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('iface', metavar='interface', type=str)
+    args = parser.parse_args()
+
     elevate(graphical=False)
-    change_mac('wlx7ca7b0c5f422')
+    change_mac(args.iface)
     p = Process(target=activate_pass)
     p.start()
     p.join()
