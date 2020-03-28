a18bf2b3adbaec154dd3c8a87d8415506dbe3dc4
milesial <milesial@users.noreply.github.com>
2020-03-27 22:36:12 -0700

    Android client app: first commit

---
+++ android_client/app/src/main/java/com/example/xfinityrefresh/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.xfinityrefresh;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.xfinityrefresh.ui.main.ChangeMacFragment;
+import com.example.xfinityrefresh.ui.main.CodeScannerFragment;
+import com.example.xfinityrefresh.ui.main.WifiConnectFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
